332c774ae0cbe657af0e6537e654885ab9ef4e8f
gaurav_mukati <gaurav.mukati99@gmail.com>
2023-10-05 11:30:37 +0530

    Initializing

--- src/main/java/com/project/taskmanagement/service/TaskService.java
+++ src/main/java/com/project/taskmanagement/service/TaskService.java
@@ -2,6 +2,8 @@ package com.project.taskmanagement.service;
 
 import com.project.taskmanagement.Entity.Task;
 import com.project.taskmanagement.repository.TaskRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +12,28 @@ public class TaskService {
 
     @Autowired
     private final TaskRepo taskRepo;
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskService.class);
 
     public TaskService(TaskRepo taskRepo) {
         this.taskRepo = taskRepo;
     }
 
     public Task createTask(Task task){
-        return taskRepo.save(task);
+
+        if(task.getTitle().isEmpty() || task.getTitle() != null){
+            throw new IllegalArgumentException("Task title should not be empty or null");
+        }
+
+        Task savedTask = taskRepo.save(task);
+
+        LOGGER.info("Task Created with ID : {}", savedTask.getId());
+
+        return savedTask;
     }
+
+//    public Task getAllTasks(){
+//
+//    }
 
 
 
